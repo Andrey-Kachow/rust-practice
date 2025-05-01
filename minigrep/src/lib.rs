4b20976353034432c4a0b3de324a8d5edc5baece
Andrey-Kachow <andr10xp@gmail.com>
2025-05-01 21:11:26 +0100

    Made test pass

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -4,7 +4,11 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    println!("With text:\n{contents}\n");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -28,7 +32,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
